72ec13275f816d3b860a8839b720241bc9ff215e
Tony <git.tonyhunt@spamgourmet.com>
2014-02-05 21:37:28 +0000

    Tests for MessageBus.HasSubscriberFor

--- Tests/MessageBusSubscriberCountTests.cs
+++ Tests/MessageBusSubscriberCountTests.cs
@@ -6,11 +6,22 @@ using MiniBus;
 
 using NUnit.Framework;
 
+using Tests.TestSupport;
+
 namespace Tests
 {
     [TestFixture]
     public class MessageBusSubscriberCountTests
     {
+        private MessageBus bus;
+
+        [TestFixtureSetUp]
+        public void SetUp()
+        {
+            bus = new MessageBus();
+            bus.SubscriberFor<Message>(m => { });
+        }
+
         [Test]
         public void StartsWithZero()
         {
@@ -32,16 +43,6 @@ namespace Tests
             bus.GetSubscriberCount.Should().Be(1);
         }
 
-        private MessageBus bus;
-        private Subscriber<Message> subscriber;
-
-        [TestFixtureSetUp]
-        public void SetUp()
-        {
-            bus = new MessageBus();
-            subscriber = bus.SubscriberFor<Message>(NoAction);
-        }
-
         [Test]
         public void DecrementsWhenSubscribersAreGarbageCollected()
         {
@@ -49,12 +50,65 @@ namespace Tests
                 Assert.Inconclusive(
                     "SubscriberCount should be 1 when subscriber added");
 
-            subscriber = null;
             GC.Collect();
 
             bus.GetSubscriberCount.Should().Be(0);
         }
+    }
 
-        private void NoAction(Message message) { }
+    [TestFixture]
+    public class MessageBusHasSubscribersTest
+    {
+        private MessageBus bus;
+
+        [TestFixtureSetUp]
+        public void SetUp()
+        {
+            bus = new MessageBus();
+        }
+
+        [Test]
+        public void FalseWithNoSubscribers()
+        {
+            bus.HasSubscriberFor<Message>()
+                .Should().BeFalse(
+                    "no subscribers have been added");
+        }
+
+        [Test]
+        public void TrueWithExactMatch()
+        {
+            bus.SubscriberFor<Message>(m => { });
+            bus.HasSubscriberFor<Message>()
+                .Should().BeTrue(
+                    "subscriber for type was added");
+        }
+
+        [Test]
+        public void FalseWithNonMatch()
+        {
+            bus.SubscriberFor<TestMessageTypeA>(m => { });
+            bus.HasSubscriberFor<TestMessageTypeB>()
+                .Should().BeFalse(
+                    "no subscriber for type was added");
+        }
+
+        [Test]
+        public void TrueWithDerivedType()
+        {
+            bus.SubscriberFor<TestMessageBase>(m => { });
+            bus.HasSubscriberFor<TestMessageTypeA>()
+                .Should().BeTrue(
+                    "subscriber for super type was added");
+        }
+
+        [Test]
+        public void FalseWithSuperType()
+        {
+            bus.SubscriberFor<TestMessageTypeA>(m => { });
+            bus.HasSubscriberFor<TestMessageBase>()
+                .Should().BeFalse(
+                    "subscriber for derived type was added");
+        }
     }
 }
